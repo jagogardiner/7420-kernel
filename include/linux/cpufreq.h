dd918210d11a2923468c8091b47beeed72ad3efd
enesuzun2002 <4lukankalar@gmail.com>
2018-02-10 08:51:22 +0000

    Import Latest Nexus CPU GOV

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -409,11 +409,10 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTEREXTREM)
-extern struct cpufreq_governor cpufreq_gov_interextrem;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interextrem)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NEXUS)
+extern struct cpufreq_governor cpufreq_gov_nexus;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nexus)
 #endif
-
 
 /*********************************************************************
  *                     FREQUENCY TABLE HELPERS                       *
